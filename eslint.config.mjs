f38e34d0febfb6c811aabfa6193616223ceecca0
ezrapetu <139527017+ezrapetu@users.noreply.github.com>
2025-07-06 19:08:31 +0700

    6-juli

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  {
+    ignores: [],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "next/no-html-link-for-pages": "off"
+    }
+    
+  }
+]
+
 
 export default eslintConfig;
